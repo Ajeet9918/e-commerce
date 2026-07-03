f70128c30bfc786de9d965bb1a73b36eda2e26d0
Ajeet <121504490+Ajeet9918@users.noreply.github.com>
2026-07-03 20:25:33 +0530

    item-update

--- client/src/components/Shop/Shop.js
+++ client/src/components/Shop/Shop.js
@@ -7,32 +7,32 @@ import ReactLoading from 'react-loading';
 
 const Shop = () => {
     TabTitle("Shop - SHOPKARO")
-    const [ menItems, setMenItems ] = useState()
-    const [ womenItems, setWomenItems ] = useState()
-    const [ kidsItems, setKidsItems ] = useState()
-    const [ loading , setLoading ] = useState(true) 
+    const [menItems, setMenItems] = useState()
+    const [womenItems, setWomenItems] = useState()
+    const [kidsItems, setKidsItems] = useState()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        axios.get("https://shema-backend.vercel.app/api/items")
+        axios.get("https://backend-paw0.onrender.com/api/items")
             .then(res => {
                 setMenItems(res.data.items.filter((item) => item.category === "men"))
-                setKidsItems(res.data.items.filter((item) => item.category === "kids" ))
+                setKidsItems(res.data.items.filter((item) => item.category === "kids"))
                 setWomenItems(res.data.items.filter((item) => item.category === "women"))
                 setLoading(false)
             })
             .catch(err => console.log(err))
         window.scrollTo(0, 0)
-    
+
     }, [])
 
-    return ( 
+    return (
         <div className="shop__contianer">
-            {loading && <ReactLoading type="balls" color='#c9ba7ecc'  height={100} width={100} className='container h-100 w-10 justify-self-center align-self-center m-auto'/>}
-            {menItems && <ShopCategory name="Men" key="men" items={menItems}/>}
-            {womenItems && <ShopCategory name="Women" key="women" items={womenItems}/>}
-            {kidsItems && <ShopCategory name="Kids" key="kids" items={kidsItems}/>}
+            {loading && <ReactLoading type="balls" color='#c9ba7ecc' height={100} width={100} className='container h-100 w-10 justify-self-center align-self-center m-auto' />}
+            {menItems && <ShopCategory name="Men" key="men" items={menItems} />}
+            {womenItems && <ShopCategory name="Women" key="women" items={womenItems} />}
+            {kidsItems && <ShopCategory name="Kids" key="kids" items={kidsItems} />}
         </div>
-     );
+    );
 }
- 
+
 export default Shop;
